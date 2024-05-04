0cb6bcfc40b206e4c871b469526c76e28244993e
Vasyl Fedinchuk <vasyl.fedinchuk@gmail.com>
2024-05-04 18:02:27 +0200

    up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Google Auth Intergration 2</p>
+        <p>Google Auth Intergration</p>
         <span>
           <GoogleLogin
             onSuccess={(credentialResponse) => {
